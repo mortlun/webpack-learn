c74280028b0908b4cd58d8b65c6ff974b9a2f4a5
mortlun <morten23_lundenes@hotmail.com>
2018-08-29 21:03:08 +0200

    started on output managment

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import _ from 'lodash';
-import './styles.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+import printMe from './print.js'
 function component() {
   let element = document.createElement('div');
+  var btn = document.createElement('button');
 
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-  console.log(Data);
+
+  btn.onclick = printMe;
+  btn.innerHTML = 'Click me and check the console!';
+  element.appendChild(btn)
+
+
+
   return element;
 }
 
