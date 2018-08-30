4b9ab515fba57c8bf6a0017e75d599f142faec0b
mortlun <morten23_lundenes@hotmail.com>
2018-08-30 22:31:56 +0200

    Learned output management

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+
 
 module.exports = {
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title : 'Output Management'
+    }),
+    new ManifestPlugin()
+
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
